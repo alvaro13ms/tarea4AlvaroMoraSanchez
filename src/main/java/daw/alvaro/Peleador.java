3b98a7857c4bee3fc0f9f7e3298377dd5d2d0cdd
Alvaro <alvaro13ms@gmail.com>
2020-04-22 13:52:39 +0200

    Añade atributos en clase modelo.

---
+++ src/main/java/daw/alvaro/Peleador.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.alvaro;
+
+/**
+ *
+ * @author Álvaro
+ */
+public class Peleador {
+
+    private int edad;
+    private int peso;
+}
