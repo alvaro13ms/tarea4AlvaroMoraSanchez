e277efc8f6c648e312ebd4ac5f672777fdde8ed5
Adrianpg <adrianperezgomezsmr@gmail.com>
2020-04-22 14:04:37 +0200

    Añade clase Main y crea dos objetos

---
+++ src/main/java/daw/alvaro/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.alvaro;
+
+/**
+ *
+ * @author adrip
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        Peleador peleador1 = new Peleador();
+        Peleador peleador2 = new Peleador(21,55);
+        
+        
+        System.out.println(peleador1);
+        System.out.println(peleador2);
+        
+        
+        
+    }
+}
